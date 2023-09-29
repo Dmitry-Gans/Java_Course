c327b2cdd58148e6ce9d9aa2815c29d5fdb01149
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-09-29 23:40:08 +0300

    Решил задачу

--- Сontrol_work/Raffle.java
+++ Сontrol_work/Raffle.java
@@ -11,6 +11,10 @@ public class Raffle {
         Toy toy4 = new Toy(4, "Кукла", 10, 20);
         Toy toy5 = new Toy(5, "Солдатик", 50, 10);
         Toy toy6 = new Toy(6, "Человек паук", 50, 60);
+        Toy toy7 = new Toy(7, "Костюм героя", 20, 30);
+        Toy toy8 = new Toy(8, "Маска", 30, 70);
+        Toy toy9 = new Toy(9, "Кубикрубик", 50, 80);
+        Toy toy10 = new Toy(10, "Диск", 60, 20);
 
         store.addToy(toy1);
         store.addToy(toy2);
@@ -18,6 +22,10 @@ public class Raffle {
         store.addToy(toy4);
         store.addToy(toy5);
         store.addToy(toy6);
+        store.addToy(toy7);
+        store.addToy(toy8);
+        store.addToy(toy9);
+        store.addToy(toy10);
 
         store.changeToyChance(1, 30);
 
